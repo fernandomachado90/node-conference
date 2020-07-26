df7ebaf4c233ad6fb128cf6e7ed1dac89b762137
Fernando Machado <lfmachado90@gmail.com>
2020-07-26 14:30:00 -0300

    sets host to localhost

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const api = require("./src/api")
 
-const host = process.env.HOST || "127.0.0.1"
 const port = process.env.PORT || "3000"
 
-api.startServer(host, port)
+api.startServer("localhost", port)
