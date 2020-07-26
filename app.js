0f3777f1ac2f7699a9d29c5793f30d37eedfd589
Fernando Machado <lfmachado90@gmail.com>
2020-07-26 14:12:14 -0300

    fetches server host and port from env vars

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const api = require("./src/api")
 
-const hostname = "127.0.0.1"
-const port = 3000
+const host = process.env.HOST || "127.0.0.1"
+const port = process.env.PORT || "3000"
 
 api.startServer(hostname, port)
